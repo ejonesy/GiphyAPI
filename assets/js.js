e4e3f15515044b79dc1a8d4218bfee660dc18411
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-04-05 13:44:06 -0700

    It works

--- assets/js.js
+++ assets/js.js
@@ -13,12 +13,6 @@ function displayFeels() {
     feelsBadMan.text(topics[i]);
     $("#feels").append(feelsBadMan);
 
-    //Why does this original code not work?
-    /*$("#feels").append("<button>" + topics[i] + "</button>");
-    //It loops through the whole array and assigns the last item to every button for some reason
-    $("button").attr("data-emotion", topics[i]);
-    console.log("Button: ", topics[i]);*/
-
   };
 
   //A click event that uses an ajax call to grab ten static gif images per button click and load them on the page
@@ -40,7 +34,9 @@ function displayFeels() {
       //Loops through every result
       for (var i = 0; i < results.length; i++) {
 
-        var emotionDiv = $("<div class = 'box'>");
+        var emotionDiv = $("<div>");
+        //Easier for the code to read by adding a class to the div this way
+        emotionDiv.addClass("box");
 
         //Each image has its GIPHY rating listed
         var rating = results[i].rating;
@@ -48,7 +44,14 @@ function displayFeels() {
 
         //Creates an image tag for each result
         var imageURL = $("<img>");
-        imageURL.attr("src", results[i].images.fixed_height.url);
+
+        //Attaches an object connecting the relevant file paths to the state of the img, to be used later in a new click event
+        imageURL.attr({
+          "src": results[i].images.fixed_height.url,
+          "data-still": results[i].images.fixed_height_still.url,
+          "data-animated": results[i].images.fixed_height.url,
+          "state": "still"
+        });
 
         //Adds the ratings and images into the div created above
         emotionDiv.append(p);
@@ -56,47 +59,41 @@ function displayFeels() {
         $("#images").prepend(emotionDiv);
         console.log("Rating: ", rating);
 
-        //Code to pause and reanimate gifs when clicked on
-        //Don't really understand how to get still images as well or where to store them or if they even need to be stored. Shouldn't it be possible to call them with .ajax like I call the moving gifs?
-
-        /*$(".gif").on("click", function() {
-          
-          var state = $(this).attr("data-state");
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        
-        });*/
-      
       };
     });
   });
   // This function handles events where the add button is clicked
-  //It's not working properly yet
   $("#add-feel").on("click", function(event) {
     event.preventDefault();
 
     // This line grabs the input from the textbox
-    var newFeels = $("#your-feels").val().trim();
+    var newFeels = $("#new-feels").val().trim();
 
-    // Adding feeling from the textbox to our array
-    topics.push(newFeels);
+    //Making sure subsequent new buttons don't get repeated    
+    if(topics.indexOf(newFeels) == -1) {
+      // Adding feeling from the textbox to our array
+      topics.push(newFeels);
 
-    // Calling displayFeels to process the array
+    // Calling displayFeels to process the array and create a new button for every new feeling added by the user
     displayFeels();
+    }
   });
 
-};
+//Code to pause and reanimate gifs when clicked on
+
+  //Selects the images div, but specifies a click event only after the img tags are created after the buttons are clicked above
+  $("#images").on("click", "img", function(){
+
+    //Checks if the img being clicked on is still or animated and changes it to the other state
+    if($(this).attr("state") === "still") {
+      $(this).attr({"src": $(this).attr("data-animated"), "state": "animated"});
+      console.log("state: still");
+    } else {
+      $(this).attr({"src": $(this).attr("data-still"), "state": "still"});
+      console.log("state: animated");
+    }
+  })
+
+}
 
 displayFeels()
-
-
-//When each image is clicked, the gif animates
-//When the moving gif is clicked, it "pauses"
-
-//A form that takes in user input to make new buttons
-
